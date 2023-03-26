55dc8811ff5d50cf31bdd8eb38498f08bb68e674
Ed <josuegomo@hotmail.com>
2023-03-26 04:50:20 -0500

    ✅ Add eslint config

--- utils/utils.js
+++ utils/utils.js
@@ -15,100 +15,100 @@ const pkg = require('../package.json')
 const infoMsg = `${colors.magenta(mainSymbols.pointer)} ${colors.yellow('Tweet CLI V2 Version:')} ${colors.blue(pkg.version)}\n\n${colors.magenta(mainSymbols.pointer)} ${colors.yellow('Click here for additional information:')} ${colors.blue('https://github.com/EDUJOS/tweet-cli-v2')}`
 
 export const info = boxen(infoMsg, {
-  title: 'Developed with love by @EdTkiere <3',
-  titleAlignment: 'center',
-  padding: 2,
-  borderColor: 'yellow',
-  borderStyle: 'round',
-  width: 60,
-  textAlignment: 'center'
+    title: 'Developed with love by @EdTkiere <3',
+    titleAlignment: 'center',
+    padding: 2,
+    borderColor: 'yellow',
+    borderStyle: 'round',
+    width: 60,
+    textAlignment: 'center'
 })
 
 export function exitProgram ({ code = 0, message = `${mainSymbols.cross} Vaya, parece que eres un completo estúpido` } = {}) {
-  outro(colors.red(message))
-  process.exit(code)
+    outro(colors.red(message))
+    process.exit(code)
 }
 
 export const Tweet = async (body, token) => {
-  try {
-    const TweetBody = {
-      TweetBody: body
+    try {
+        const TweetBody = {
+            TweetBody: body
+        }
+        // Api status: Off
+        const results = await fetch('https://web-start.up.railway.app/api/singletweet', {
+            method: 'POST',
+            body: JSON.stringify(TweetBody),
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        })
+        const data = await results.json()
+        return data
+    } catch (err) {
+        console.error(err)
+        return err
     }
-    // Api status: Off
-    const results = await fetch('https://web-start.up.railway.app/api/singletweet', {
-      method: 'POST',
-      body: JSON.stringify(TweetBody),
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`
-      }
-    })
-    const data = await results.json()
-    return data
-  } catch (err) {
-    console.error(err)
-    return err
-  }
 }
 
 export const login = async () => {
-  const sp = spinner()
-  try {
-    const folderPath = path.join(__dirname, 'User-Credentials')
-    const filePath = path.join(folderPath, 'user.json')
-    if (!fs.existsSync(folderPath)) {
-      const usernameCmd = await text({
-        message: 'Parece que no te has logeado o tu sesión ha expirado, por favor inicia sesión... 🍟',
-        placeholder: 'Ingresa tu Username aquí 👀',
-        validate (value) {
-          if (value === 0) return `${colors.yellow(`${mainSymbols.cross} Lo siento, no puedes enviar un string vacío`)}`
+    const sp = spinner()
+    try {
+        const folderPath = path.join(__dirname, 'User-Credentials')
+        const filePath = path.join(folderPath, 'user.json')
+        if (!fs.existsSync(folderPath)) {
+            const usernameCmd = await text({
+                message: 'Parece que no te has logeado o tu sesión ha expirado, por favor inicia sesión... 🍟',
+                placeholder: 'Ingresa tu Username aquí 👀',
+                validate (value) {
+                    if (value === 0) return `${colors.yellow(`${mainSymbols.cross} Lo siento, no puedes enviar un string vacío`)}`
+                }
+            })
+            if (isCancel(usernameCmd)) exitProgram()
+            const passwordCmd = await text({
+                message: 'Introduce tu contraseña 🔐',
+                placeholder: 'Ingresa tu contraseña aquí 👀',
+                validate (value) {
+                    if (value === 0) return `${colors.yellow(`${mainSymbols.cross} Lo siento, no puedes enviar un string vacío`)}`
+                }
+            })
+            if (isCancel(passwordCmd)) exitProgram()
+            const userBody = {
+                username: usernameCmd,
+                password: passwordCmd
+            }
+            sp.start(`${colors.yellow('Iniciando sesión')}`)
+            const data = await getToken(userBody)
+            if (data.error === 'Invalid user or password') {
+                sp.stop(`${colors.red(`${mainSymbols.cross}`)} ${colors.yellow(`Ups... Parece que tus credenciales son inválidas.\nIntenta ejecutar: ${colors.magenta(`edtba ${mainSymbols.arrowRight} npx edtba`)} para intentarlo una vez más!`)} 😅`)
+                exitProgram()
+            } else {
+                sp.stop(`${colors.green(`${mainSymbols.tick} Bienvenido de vuelta`)} ${colors.magenta(usernameCmd)}✨`)
+                const UserCredentials = {
+                    username: data.username,
+                    password: passwordCmd,
+                    token: data.token
+                }
+                sp.start(`${colors.yellow('Guardando credenciales 📩')}`)
+                fs.mkdirSync(folderPath)
+                fs.writeFileSync(filePath, JSON.stringify(UserCredentials))
+                sp.stop(`${colors.green(`${mainSymbols.tick}`)} ${colors.magenta('Tus credenciales han sido guardadas con éxito')}🔐`)
+            }
         }
-      })
-      if (isCancel(usernameCmd)) exitProgram()
-      const passwordCmd = await text({
-        message: 'Introduce tu contraseña 🔐',
-        placeholder: 'Ingresa tu contraseña aquí 👀',
-        validate (value) {
-          if (value === 0) return `${colors.yellow(`${mainSymbols.cross} Lo siento, no puedes enviar un string vacío`)}`
-        }
-      })
-      if (isCancel(passwordCmd)) exitProgram()
-      const userBody = {
-        username: usernameCmd,
-        password: passwordCmd
-      }
-      sp.start(`${colors.yellow('Iniciando sesión')}`)
-      const data = await getToken(userBody)
-      if (data.error === 'Invalid user or password') {
-        sp.stop(`${colors.red(`${mainSymbols.cross}`)} ${colors.yellow(`Ups... Parece que tus credenciales son inválidas.\nIntenta ejecutar: ${colors.magenta(`edtba ${mainSymbols.arrowRight} npx edtba`)} para intentarlo una vez más!`)} 😅`)
+    } catch (err) {
+        console.log(err)
+        sp.stop(`${colors.red('Vaya, parece que ha ocurrido un error inesperado...')}😅`)
         exitProgram()
-      } else {
-        sp.stop(`${colors.green(`${mainSymbols.tick} Bienvenido de vuelta`)} ${colors.magenta(usernameCmd)}✨`)
-        const UserCredentials = {
-          username: data.username,
-          password: passwordCmd,
-          token: data.token
-        }
-        sp.start(`${colors.yellow('Guardando credenciales 📩')}`)
-        fs.mkdirSync(folderPath)
-        fs.writeFileSync(filePath, JSON.stringify(UserCredentials))
-        sp.stop(`${colors.green(`${mainSymbols.tick}`)} ${colors.magenta('Tus credenciales han sido guardadas con éxito')}🔐`)
-      }
     }
-  } catch (err) {
-    console.log(err)
-    sp.stop(`${colors.red('Vaya, parece que ha ocurrido un error inesperado...')}😅`)
-    exitProgram()
-  }
 }
 
 export const getToken = async (userBody) => {
-  // Api status: Off
-  const results = await fetch('https://web-start.up.railway.app/api/login', {
-    method: 'POST',
-    body: JSON.stringify(userBody),
-    headers: { 'Content-Type': 'application/json' }
-  })
-  const data = await results.json()
-  return data
+    // Api status: Off
+    const results = await fetch('https://web-start.up.railway.app/api/login', {
+        method: 'POST',
+        body: JSON.stringify(userBody),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    const data = await results.json()
+    return data
 }
