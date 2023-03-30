713b2a8b514448e82ac084995a7ed15e365bae83
Ed <josuegomo@hotmail.com>
2023-03-29 20:04:32 -0500

    Add random placeholder to constants file

---
+++ utils/constants.js
@@ -0,0 +1,10 @@
+const placeholders = [
+  'A chucho le gusta el pene, según los expertos...',
+  'Do u like big diks?',
+  'I\'m feeling blue',
+  'Quién pa\' peliculiar?🥵'
+]
+
+const random = Math.floor(Math.random()*(placeholders.length))
+
+export const RANDOM_PLACEHOLDER = `${placeholders[random]}`
