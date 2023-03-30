e18892d49f8a15312f5170cc2a3bc93695f8d5da
Ed <josuegomo@hotmail.com>
2023-03-29 20:33:10 -0500

    🛠  refactor: Little code refactoring

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { setTimeout as sleep } from 'node:timers/promises'
 import colors from 'picocolors'
 import { mainSymbols } from 'figures'
 import { exitProgram, Tweet, info, login, getToken } from '../utils/utils.js'
+import { RANDOM_PLACEHOLDER } from '../utils/constants.js'
 import path from 'path'
 import fs from 'fs'
 import { fileURLToPath } from 'url'
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+const sp = spinner()
 
 async function main () {
   console.clear()
@@ -28,8 +31,7 @@ async function main () {
     await login()
     const publishTweet = await text({
       message: colors.blue('Ingresa el cuerpo del Tweet a publicar'),
-      placeholder: 'A chucho le gusta el pene, según los expertos...',
-      initialValue: 'A chucho le gusta el pene, según los expertos...',
+      placeholder: RANDOM_PLACEHOLDER,
       validate (value) {
         if (value.length > 200) return `${colors.red(`${mainSymbols.cross} Asegúrate de no ingresar más de 100 caracteres`)}`
         if (value.length === 0) return `${colors.red(`${mainSymbols.cross} Uhmm, asegúrate de ingresar al menos más de 2 caracteres`)}`
@@ -48,9 +50,8 @@ async function main () {
     if (tweetConf === false) {
       exitProgram()
     } else {
-      const sp = spinner()
-      sp.start(`${colors.yellow('Estamos procesando tu solicitud')}`)
-      const ruta = path.join(__dirname, '.././utils/User-Credentials')
+      sp.start(`${colors.yellow('Procesando tu solicitud')}`)
+      const ruta = path.join(__dirname, '.././cli-config/credentials')
       const filePath = path.join(ruta, 'user.json')
       const file = require(filePath)
       const token = file.token
@@ -63,18 +64,17 @@ async function main () {
         outro(`${colors.bold(colors.magenta('Tweet Body:'))} ${colors.italic(colors.blue(data.tweetBody))}\n   ${colors.bold(colors.magenta('Tweet Url:'))} ${colors.blue(data.url)}`)
       }
     }
-  } else {
-    const sp = spinner()
+  }
+  if (tweetCmd === 'tweetInfo') {
     sp.start(colors.magenta('Procesando solicitud'))
     await sleep(2000)
-    sp.stop(colors.red('Lo siento, esta función no está diponible por lo que el proyecto apenas empieza.'))
+    sp.stop(colors.red('Lo siento, esta función no está diponible por el momento.'))
     outro(colors.magenta('Pero no te preocupes que pronto estará disponible!!'))
   }
 }
 
 const tokenExpired = async () => {
-  const sp = spinner()
-  const ruta = path.join(__dirname, '.././utils/User-Credentials')
+  const ruta = path.join(__dirname, '.././cli-config/credentials')
   const filePath = path.join(ruta, 'user.json')
   const file = require(filePath)
   sp.stop(`${colors.red('Vaya, parece que tu sesión ha caducado')}😭`)
@@ -107,7 +107,7 @@ const tokenExpired = async () => {
       password: file.password,
       token: tokenData.token
     }
-    fs.writeFileSync(filePath, JSON.stringify(UserCredentials))
+    fs.writeFileSync(filePath, JSON.stringify(UserCredentials, null, '\t'))
     sp.stop(`${colors.green(mainSymbols.tick)}`)
     outro(`${colors.magenta('Tus credenciales han sido actualizadas con éxito')}🔐`)
   }
