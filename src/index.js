1f773ff15dde79c73647095036125899aea8e450
Ed <josuegomo@hotmail.com>
2024-01-17 22:24:38 -0500

    ✅ add i18n config

--- src/index.js
+++ src/index.js
@@ -11,6 +11,31 @@ import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
+const { I18n } = require('i18n')
+const i18n = new I18n()
+
+i18n.configure({
+  locales: ['es', 'en'],
+  directory: path.join(__dirname, '../locales'),
+  defaultLocale: 'en',
+  retryInDefaultLocale: true,
+  objectNotation: true,
+  register: global,
+  logWarnFn: function (msg) {
+    console.log('ALERTA' + msg)
+  },
+  LogErrorFn: function (msg) {
+    console.log('Error' + msg)
+  },
+  missingKeyFn: function (locate, value) {
+    return value
+  },
+  mustacheConfig: {
+    tags: ['{{', '}}'],
+    disable: false
+  }
+})
+
 const sp = spinner()
 
 async function main () {
